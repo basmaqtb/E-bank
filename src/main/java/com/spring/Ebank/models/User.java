a3409ae9e24ac872bb6de8a9b074c5150e49aea6
basmaqtb <qotbibasma8@gmail.com>
2024-07-05 10:55:58 +0100

    création du projet springboot

---
+++ src/main/java/com/spring/Ebank/models/User.java
@@ -0,0 +1,32 @@
+package com.spring.Ebank.models;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id_user;
+    private  String fullname_user ;
+    private String email_user ;
+    private String password_user ;
+
+
+}
